0fb38db233e915222752ce2b0d73a81508cb7784
mandriv <wojciech.cichoradzki@gmail.com>
2019-10-22 03:59:04 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Test Case Prioritazation',
     version='0.1',
-    packages=['project',],
+    packages=['test-case-prioritazation'],
     license='MIT',
     long_description=open('README.md').read(),
 )
